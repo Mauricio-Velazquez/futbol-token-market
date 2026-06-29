37184a35eaf4cb12831b3f494375baa337aedb7e
Fer Urquijo <jfurquijo@gmail.com>
2026-06-29 00:06:16 +0000

    metrics endpints

--- src/main/java/com/futbol/tokenmarket/config/OpenApiConfig.java
+++ src/main/java/com/futbol/tokenmarket/config/OpenApiConfig.java
@@ -7,11 +7,28 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi applicationApi() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi monitoringApi() {
+        return GroupedOpenApi.builder()
+                .group("monitoring")
+                .pathsToMatch("/actuator/**")
+                .build();
+    }
 
     @Bean
     public OpenAPI openAPI() {
